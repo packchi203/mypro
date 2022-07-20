1732a365d739f5e4c7425f6ecbedaa8733c7aabc
packchi203 <bachnvth2108029@fpt.edu.vn>
2022-07-20 15:16:11 +0700

    ad
    ad

--- backend.php
+++ backend.php
@@ -1,39 +1,44 @@
 <?php
-$connect = mysqli_connect("localhost","root"," ","mypro_crud");
+$connect = mysqli_connect("localhost","root",'',"mypro_crud");
 
-// check connection
+//check connection
 if($connect === false){
-    die("ERROR: Cloud not connect. ". mysqli_connect_error());
-
+    die("ERROR: Cloud not connect." .mysqli_connect_error());
 }
-
 if(isset($_REQUEST["term"])){
-
     //prepare a select statement
-    $sql = "SELECT * FROM Student WHERE name LIKE ?";
+   $sql ="SELECT * FROM student WHERE name LIKE ?";
 
-    if($stmt = mysqli_prepare($connect, $sql)){
-        mysqli_stmt_bind_param($stmt, "s",$param_term);
 
-        $param_term = $_REQUEST["term"] . '%';
-        
-        if(mysqli_stmt_execute($stmt)){
-            $result = mysqli_stmt_get_result($stmt);
+  if($stmt = mysqli_prepare($connect, $sql)) {
+      //bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "s", $param_term);
 
-            if(mysqli_num_rows($result) > 0){
-                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                    echo "<p>" . $row["name"] ."</p>";
-                }
-            } else{
-                echo "<p> No matches found</p>";
-            }
-        } else{
-            echo "ERROR: Cloud not able to execute $sql. ". mysqli_error($link);
-        }
-    }
+      //set parameters
+      $param_term = $_REQUEST["term"] . '%';
+      //Attemp to execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+          $result = mysqli_stmt_get_result($stmt);
+          //check number of rows in the result set
+          if (mysqli_num_rows($result) > 0) {
+              //fetch result rows as an associative array
+              while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                  echo "<p>" . $row["name"] . "</p>";
+
+              }
+          } else {
+              echo "<p>No matches found</p>";
+          }
+      } else {
+          
+          echo "ERROR: Could not able to execute $sql." . mysqli_error($link);
+
+      }
+  }
+    //close statement
     mysqli_stmt_close($stmt);
 }
 
-
+//close connection
 mysqli_close($connect);
 ?>
